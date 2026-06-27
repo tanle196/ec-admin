462d52637f16a216211ac472c1935e08cdf13a6e
Tan - Personal <tan-personal@Mac-mini-cua-Tan.local>
2026-06-27 10:10:38 +0700

    Change import query client

--- src/features/roles/hooks/useUpdateRole.ts
+++ src/features/roles/hooks/useUpdateRole.ts
@@ -1,10 +1,11 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { type UpdateRoleDto } from '@/api/main'
-import { queryClient } from '@/lib/query-client'
 import { roleService } from '../api/roleService'
 import { roleKeys } from '../queryKeys'
 
 export const useUpdateRole = () => {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: ({ id, body }: { id: string; body: UpdateRoleDto }) =>
       roleService.update({ path: { id }, body }),
